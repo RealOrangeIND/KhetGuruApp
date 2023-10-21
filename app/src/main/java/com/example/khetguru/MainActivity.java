a54fe3d242a2ebb0362ec6cce3bf4536c76b4ae3
RealOrangeIND <realorangeind@gmail.com>
2023-10-21 19:37:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/khetguru/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.khetguru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+
+        // Set a custom WebViewClient
+        webView.setWebViewClient(new MywebClient());
+
+        // Enable JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Enable storage and cache
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+
+        // Load your website
+        webView.loadUrl("https://khetguru.netlify.app/");
+    }
+
+    private class MywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            // Handle URL loading as per your requirements
+            // If you want to open links within your WebView, handle them here.
+            // If not, return false to open external links in the device's browser.
+            // For example, to open all links within the WebView:
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.isFocused() && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
